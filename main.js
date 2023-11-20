ad28d1bce30421f1fda13efa2b05d1051abc5aca
Alma Hernández <150403661+HernandezAlma@users.noreply.github.com>
2023-11-19 22:08:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #fb4993;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #fb4993;">Estudio Ingeniería en tecnologías de software y programación web.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
